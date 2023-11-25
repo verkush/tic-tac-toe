4bee2aa7f661d699cdf21e78792eb08f64b9d95d
Ayush Kumar Verma <ayushkumarverma97@outlook.com>
2023-11-25 23:07:07 +0530

    Add player functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import './styles.scss';
 import Board from './components/Board';
+import { calculateWinner } from './winner';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsNext] = useState(false);
+
+  const winner = calculateWinner(squares);
+  const nextPlayer = isXNext ? 'X' : 'O';
+  const statusMessage = winner
+    ? `The winner is ${winner}`
+    : `Next player is ${nextPlayer}`;
+
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winner) {
+      return;
+    }
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+    setIsNext(currentIsXNext => !currentIsXNext);
+  };
+
   return (
     <div className="app">
-      <Board />
+      <h2>{statusMessage}</h2>
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
